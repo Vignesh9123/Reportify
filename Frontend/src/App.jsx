c8fbdf38d39bff8f9579a1439d9d78f4fea33fc9
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-02-07 22:48:07 +0530

    Update Google login API endpoint to use production URL

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -59,7 +59,7 @@ function App() {
         name: user.displayName,
         email: user.email,
       };
-      const signInPromise = axios.post("/api/auth/google-login", userData, {
+      const signInPromise = axios.post("https://reportify-backend.vercel.app/api/auth/google-login", userData, {
         headers: {
           "Content-Type": "application/json",
         },
