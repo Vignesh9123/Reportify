12776dd072c7a68d91d05b63d70b9b9636efb18a
Vignesh D <vignesh.d9123@gmail.com>
2025-02-07 22:05:28 +0530

    sample route

--- Backend/src/routes/auth.route.ts
+++ Backend/src/routes/auth.route.ts
@@ -1,10 +1,15 @@
 import { googleLogin, currentUser } from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 import { Router } from "express";
+import User from "../models/user.model";
 
 const router = Router();
 
 router.post("/google-login", googleLogin);
 router.route("/current-user").get(authMiddleware, currentUser);
+router.route("/sample").get(async(req, res)=>{
+    const user = await User.findOne({email: "vignesh.d9123@gmail.com"});
+    res.status(200).json(user)
+})
 
 export default router;
