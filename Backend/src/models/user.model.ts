654cd6cd4918b20cb5bd315f6acdb0424fda1762
Vignesh D <vignesh.d9123@gmail.com>
2025-02-10 13:00:53 +0530

    Add monthly credit reset mechanism with date-fns
    - Introduce creditsResetDate field in user model
    - Create calculateResetDate utility function using date-fns
    - Update credit reset logic to use new creditsResetDate
    - Modify user schema to set default reset date one month after account creation

--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { UserType } from "../config/types";
-
+import { calculateResetDate } from "../utils/resetDate";
 const userSchema = new mongoose.Schema({
     name: { 
         type: String, 
@@ -33,6 +33,10 @@ const userSchema = new mongoose.Schema({
     maxCredits: {
         type: Number,
         default: 5
+    },
+    creditsResetDate: {
+        type: Date,
+        default: calculateResetDate(new Date())
     }
 }, { timestamps: true });
 
