75920aaf540d9cebb2f90fce078a7ae724b1a197
Vignesh D <vignesh.d9123@gmail.com>
2025-07-25 17:09:09 +0530

    Add send email route and handler

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authRouter from "./auth.route";
 import reportRouter from "./report.route";
 import contentRouter from "./content.route";
+import emailRouter from "./email.routes";
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/report", reportRouter);
 router.use("/content", contentRouter);
+router.use("/email", emailRouter);
 
 export default router;
