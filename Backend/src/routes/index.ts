19c0cfcd529280620e3e9ef083bc3dc278eeed49
Vignesh D <vignesh.d9123@gmail.com>
2025-07-25 17:27:21 +0530

    Revert email commit

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 import authRouter from "./auth.route";
 import reportRouter from "./report.route";
 import contentRouter from "./content.route";
-import emailRouter from "./email.routes";
 const router = Router();
 
 router.use("/auth", authRouter);
 router.use("/report", reportRouter);
 router.use("/content", contentRouter);
-router.use("/email", emailRouter);
 
 export default router;
