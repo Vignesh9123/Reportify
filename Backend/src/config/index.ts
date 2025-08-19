bafccc05f911e6cf5915b3a417cab04acfcaebd9
Vignesh D <vignesh.d9123@gmail.com>
2025-08-19 15:38:49 +0530

    Update to 2.5 flash

--- Backend/src/config/index.ts
+++ Backend/src/config/index.ts
@@ -9,7 +9,7 @@ export const config = {
     GEMINI_API_KEY_2: String(process.env.GEMINI_API_KEY_2),
     CLIENT_URL: String(process.env.CLIENT_URL),
     JWT_SECRET: String(process.env.JWT_SECRET),
-    'GEMINI1.5':'gemini-1.5-flash',
+    'GEMINI2.5':'gemini-2.5-flash',
     'GEMINI2.0':'gemini-2.0-flash',
     ADMIN_KEY: String(process.env.ADMIN_KEY),
     UPLOADTHING_TOKEN: String(process.env.UPLOADTHING_TOKEN),
